8bcb08b3b510f583f1b4d88fb3db05e9b60f4bf7
srestamalla <mallasresta@gmail.com>
2023-05-22 08:02:35 +0545

    angular crash course

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
+
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css'],
+})
+export class TaskComponent {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+  
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>(this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+}
